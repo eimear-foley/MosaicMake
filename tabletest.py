20f6e39c7f2acafb84c8de134b8959df3296baba
Alessia Nardotto <alessianard8@gmail.com>
2016-06-10 16:16:33 +0100

    Update tabletest.py

--- tabletest.py
+++ tabletest.py
@@ -4,29 +4,25 @@ from math import sqrt
 lst1 = [(255,233,100),(100,122,255),(255,183,100)]
 lst2 = [(149,72,194),(254,99,200),(194,137,72)]
 
-
-
 def ColourDiff(tup1,tup2):
     r1,g1,b1 = tup1
     r2,g2,b2 = tup2
-    rChange = (r1 - r2)**2
-    gChange = (g1 - g2)**2
-    bChange = (b1 - b2)**2
-    rMean = (r1 + r2)//2
-    return round(sqrt( ( 2 + rMean//256 )*rChange + 4*gChange + (2 + (255 - rMean)//256)* bChange ))
+    rChange = (r1 - r2) ** 2
+    gChange = (g1 - g2) ** 2
+    bChange = (b1 - b2) ** 2
+    rMean = (r1 + r2) // 2
+    return int(round(sqrt((2 + rMean // 256 ) * rChange + 4 * gChange + (2 + (255 - rMean) // 256) * bChange)))
 
-
-
-def DiffTable(lst1, lst2):
+def DiffTable(lst1,lst2):
     table = []
     for colour1 in lst1:
         row = []
         for colour2 in lst2:
-            row += [ColourDiff(colour1,colour2)]
+            row += [ColourDiff(colour1, colour2)]
         table += [row]
     return table    
 
-DiffTable(lst1, lst2)
+print(DiffTable(lst1, lst2))
 
 
 
