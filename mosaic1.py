7c356f5e2fa4f0e714823a7df106f69759876dc0
Alessia Nardotto <alessianard8@gmail.com>
2016-06-13 12:04:53 +0100

    Update mosaic1.py

--- mosaic1.py
+++ mosaic1.py
@@ -2,52 +2,55 @@ from Numberjack import *
 from ColourCost import *
 
 def flatten(matrix):
-
-      matches = []
-      for row in range(len(matrix)):
-            for item in range(len(matrix[row])):
-                  if matrix[row][item].get_value() == 1:
-                        matches += [(row, item)]
-      return matches
+	
+    matches = []
+    for row in range(len(matrix)):
+        for item in range(len(matrix[row])):
+            if matrix[row][item].get_value() == 1:
+                matches += [(row, item)]
+    return matches
                         
 
 def get_model():
-      N = len(param['inputtable'])
-      inputtable = param['inputtable']
-      #output = VarArray(N, N)
-      matrix = Matrix(N, N) #NxN Matrix of booleans
-      
-      #The overall cost of the chosen cells in 'matrix' times their corresponding cost in the 'inputtable'
-      cost = Sum([Sum(matrix[i], inputtable[i]) for i in range(N)]) 
+	
+    N = len(param['inputtable'])
+    inputtable = param['inputtable']
+    # NxN Matrix of booleans
+    matrix = Matrix(N, N)
+    # The overall cost of the chosen cells in 'matrix' times their corresponding cost in the 'inputtable'
+    cost = Sum([Sum(matrix[i], inputtable[i]) for i in range(N)]) 
 
-      model = Model(
-            Minimize(cost), #finding minimum cost
-            [Sum(row) == 1 for row in matrix.row], #only one chosen value per row
-            [Sum(col) <= 1 for col in matrix.col] #only one chosen value per column
-            )
+    model = Model(
+    	# finding minimum cost
+        Minimize(cost),
+        # only one chosen value per row
+        [Sum(row) == 1 for row in matrix.row],
+        # only one chosen value per column
+        [Sum(col) <= 1 for col in matrix.col]
+    )
 
-      return matrix, cost, model
+    return matrix, cost, model
 
 def solve(param):
 
-      matrix, cost, model = get_model()
+    matrix, cost, model = get_model()
 
-      solver = model.load(param['solver'])
-      solver.setVerbosity(param['verbose'])
-      solver.setTimeLimit(param['tcutoff'])
-      solver.solve()
+    solver = model.load(param['solver'])
+    solver.setVerbosity(param['verbose'])
+    solver.setTimeLimit(param['tcutoff'])
+    solver.solve()
 
-      if solver.is_sat():
-            print(str(matrix))
-            print(flatten(matrix))
-            print("Time:", solver.getTime())
-      elif solver.is_unsat():
-            print('Unsatisfiable')
-      else:
-            print('Timed out')
+    if solver.is_sat():
+        print(str(matrix))
+        print(flatten(matrix))
+        print("Time:", solver.getTime())
+    elif solver.is_unsat():
+        print('Unsatisfiable')
+    else:
+        print('Timed out')
 
 if __name__ == '__main__':
 
-      default = {'solver': 'Mistral', 'verbose': 0, 'tcutoff': 30, 'inputtable': DiffTable('RGBValues.txt')}
-      param = input(default)
-      solve(param)
+    default = {'solver': 'Mistral', 'verbose': 0, 'tcutoff': 30, 'inputtable': DiffTable('RGBValues.txt')}
+    param = input(default)
+    solve(param)
