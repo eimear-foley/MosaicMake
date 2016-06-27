d35bbd11d89ae133b8d023be20e3a1e8c76464c7
Alessia Nardotto <alessianard8@gmail.com>
2016-06-27 11:00:06 +0100

    Update image_processing.py

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 from os import listdir
 from os.path import isfile, join
 from ColourCost import *
@@ -15,7 +15,7 @@ def SplitImage(img, N):
     if imgwidth > imgheight:
         diff = imgwidth - imgheight
         h = imgheight - N * int(imgheight // N)
-        #resized = img.crop(((diff + h)//2,  h//2, imgheight + diff - h,imgheight - h//2))
+        # resized = img.crop(((diff + h)//2,  h//2, imgheight + diff - h,imgheight - h//2))
         resized = im.crop((0, 0, imgheight - h, imgheight - h))
 
     elif imgwidth < imgheight:
@@ -31,7 +31,7 @@ def SplitImage(img, N):
 
     im2 = Image.open(mypath + "resized.jpeg")
     w2, h2 = im2.size
-
+    print('CREATING TILES')
     rgb_values = get_rgb('resized.jpeg', N, w2, h2)
 
     rgbimg = ResizeImg(w2 // N)
@@ -44,7 +44,6 @@ def SplitImage(img, N):
 def most_frequent_color(lst, folder):
     # Finds most frequntly occuring color
     # in each image in the list
-
     rgb = []
     for image in lst:
         img = Image.open(mypath + folder + image)
@@ -63,13 +62,12 @@ def ResizeImg(tileWidth):
     # Resizes all images in lst to the size of
     # the split tiles of the original image
     # returns list of tuples containing rgb values
-    print('TILE WIDTH: ', tileWidth)
+
     lst = [f for f in listdir(mypath + "pictures/") if isfile(
         join(mypath + "pictures/", f)) if not f.endswith('.DS_Store')]
     lst.sort()
-    print('RESIZING IMAGES')
     lst2 = []
-
+    print('RESIZING TILES')
     for im in lst:
         lst2 += most_frequent_color([im], 'pictures/')
         img = Image.open(mypath + "pictures/" + im)
@@ -78,7 +76,6 @@ def ResizeImg(tileWidth):
         quality_val = 100
         img.save(mypath + 'pictures/' + im, subsampling=0, quality=quality_val)
         # saves resized images in mypath
-        #img.save(mypath + 'pictures/' + im)
 
     return lst2
 
@@ -87,19 +84,18 @@ def grid(nj, orgimage):
 
     lst = [f for f in listdir(mypath + "pictures/") if isfile(
         join(mypath + "pictures/", f)) if f != '.DS_Store']
+    print('SORTING NOW')
     lst.sort()
-    print("LIST WITH IMAGES")
+    print('SORTING DONE')
     tile = Image.open(mypath + "pictures/" + lst[0])
     w, h = tile.size  # width and height of tile
-    print(w, h)
     orgimage = Image.open(orgimage)
     total_w, total_h = orgimage.size
-    print(total_w, total_h)
     x = 0
     y = 0
     t = 0
     result = Image.new('RGB', (total_w, total_h))  # new image
-    print(nj)
+    # print(nj)
     while y + h <= total_h and t < len(nj):
         x = 0
         while x + w <= total_w:
@@ -113,9 +109,12 @@ def grid(nj, orgimage):
     result.save(mypath + 'final.jpeg')
     result.show()
     im2 = Image.open(mypath + 'resized.jpeg')
-    res = Image.blend(result, im2, 0.25)
+    im3 = im2.filter(ImageFilter.EDGE_ENHANCE_MORE)
+    im3.save(mypath + 'fe.jpeg')
+    im3.show()
+    res = Image.blend(result, im3, 0.25)
     res.save(mypath + 'filter.jpeg')
     res.show()
 
-si = SplitImage('paris.jpg', 30)
+si = SplitImage('me.jpg', 30)
 grid(Final(si), 'resized.jpeg')
