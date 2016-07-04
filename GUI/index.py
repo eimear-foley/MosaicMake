831b477486e429a9936a5a2393dbbf8acab8bb48
Alessia Nardotto <alessianard8@gmail.com>
2016-07-04 09:48:32 +0100

    Add files via upload

---
+++ GUI/index.py
@@ -0,0 +1,106 @@
+#!/usr/local/bin/python3
+
+print("""
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by TEMPLATED
+http://templated.co
+Released for free under the Creative Commons Attribution License
+
+Name       : RedMarket 
+Description: A two-column, fixed-width design with dark color scheme.
+Version    : 1.0
+Released   : 20140101
+
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+<!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+
+</head>
+<body>
+<div id="header-wrapper">
+	<div id="header" class="container">
+		<div id="logo">
+			<h1><a href="#">Splitkit</a></h1>
+		</div>
+		<div id="menu">
+			<ul>
+				<li class="active"><a href="#" accesskey="1" title="">Homepage</a></li>
+				<li><a href="cs1dev.ucc.ie/~ctb1/cgi-bin/splitkit/try.py" accesskey="2" title="">Try it</a></li>
+				<li><a href="#" accesskey="3" title="">About Us</a></li>
+				<li><a href="#" accesskey="4" title="">Careers</a></li>
+				<li><a href="#" accesskey="5" title="">Contact Us</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="banner" class="container"><a href="cs1dev.ucc.ie/~ctb1/cgi-bin/splitkit/try.py"><span>Try it</span></a></div>
+</div>
+<div id="wrapper">
+	<div id="staff" class="container">
+		<div class="title">
+			<h2>Mosaic examples</h2>
+			<span>Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum</span> </div>
+		<div class="boxA"><img src="images/pic01.jpg" width="300" height="450" alt="" /></div>
+		<div class="boxB"><img src="images/pic02.jpg" width="300" height="450" alt="" /></div>
+		<div class="boxC"><img src="images/pic03.jpg" width="300" height="450" alt="" /></div>
+	</div>
+	<div id="page" class="container">
+		<div class="boxA">
+			<h2>Fusce ultrices<br />
+				<span>fringilla metus</span></h2>
+			<p>Donec leo, vivamus fermentum nibh in augue praesent a lacus at urna congue rutrum.</p>
+			<ul class="style4">
+				<li class="first"><a href="#">Consectetuer adipiscing elit</a></li>
+				<li><a href="#">Metus aliquam pellentesque</a></li>
+				<li><a href="#">Proin gravida orci porttitor</a></li>
+			</ul>
+		</div>
+		<div class="boxB">
+			<h2>Follow<br /><span> us on</span></h2>
+			<ul class="style3">
+				<li class="first">
+					<p class="date"><a href="#"><b>f</b></a></p>
+					<h3>Facebook</h3>
+					<p><a href="http://www.facebook.com">splitkit</a></p>
+				</li>
+				<li>
+					<p class="date"><a href="#"><b>i</b></a></p>
+					<h3>Instagram</h3>
+					<p><a href="http://www.instagram.com">@splitkit</a></p>
+				</li>
+			</ul>
+		</div>
+		<div class="boxC">
+			<h2>Developers<br />
+				<span>augue sit amet</span></h2>
+			<ul class="style4">
+				<li class="first"><a href="#">Alessia Nardotto</a></li>
+				<li><a href="#">Eimear Foley</a></li>
+				<li><a href="#">Catherine Bowen</a></li>
+				<li><a href="#">Claire Foran</a></li>
+				<li><a href="#">Gabrielle Stephanov</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="welcome" class="container">
+		<div class="title">
+			<h2>Welcome to Splitkit</h2>
+		</div>
+		<p>This is <strong>Splitkit</strong>, a free, fully standards-compliant CSS template designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. The photos in this template are from <a href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under the <a href="http://templated.co/license">Creative Commons Attribution</a> license, so you're pretty much free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have fun :) </p>
+	</div>
+</div>
+<div id="copyright" class="container">
+	<p>&copy; Splitkit. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
+</body>
+</html>
+""")
