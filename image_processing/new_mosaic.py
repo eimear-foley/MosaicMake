8d2039dd37e561339e34205593470ba420e7db28
Alessia Nardotto <alessianard8@gmail.com>
2016-07-05 10:33:46 +0100

    Update new_mosaic.py

--- image_processing/new_mosaic.py
+++ image_processing/new_mosaic.py
@@ -2,16 +2,20 @@ from PIL import Image, ImageFilter
 from os import listdir
 from os.path import isfile, join
 from ColourCost import *
-from new_nj import *
-from mergesort import *
-import timeit
+from mosaic1 import *
+from get_rgb import *
 from api import *
+
 mypath = '/Users/claire/mosaic/'
 
 
 def SplitImage(img, N):
     print("SPLIT IMAGE")
-    im = Image.open(img)
+    try:
+		im = Image.open(img)
+	except FileNotFoundError:
+		print('Error opening main image')
+		exit()
     imgwidth, imgheight = im.size
     if imgwidth > imgheight:
         diff = imgwidth - imgheight
@@ -28,78 +32,36 @@ def SplitImage(img, N):
 
     print('MAIN IMAGE RESIZED')
     resized.save(mypath + "resized.jpeg")
-
     im2 = Image.open(mypath + "resized.jpeg")
     w2, h2 = im2.size
-    
-    rgb_value = get_rgb('resized.jpeg', N, w2, h2)
     tileWidth = w2 // N
+    rgb_value = get_rgb('resized.jpeg', N, w2, h2)
+    # resize all the images used in the final mosaic
     get_photos(tileWidth)
-    
     mosaic_images = [f for f in listdir(mypath + "mosaic_photos/") if isfile(
         join(mypath + "mosaic_photos/", f)) if not f.endswith('.DS_Store')]
-    mergeSort(mosaic_images)
-    rgbimg = [get_average_color(0, 0, tileWidth, mypath + 'mosaic_photos/'+ img) for img in mosaic_images]
-    
+    mosaic_images.sort()
     # a list of tuples containging rgb values are stored in variable 'rgbimg'
     # returns a list of rgb values in tuples
-    
-    print(rgb_value, rgbimg)
+    rgbimg = []
+    for img in mosaic_images:
+        try:
+            rgbimg += [get_average_color(0, 0, tileWidth, mypath + 'mosaic_photos/'+ img)]
+        except IOError:
+            print('Problem with %s' % (img))
+			continue
     return rgb_value, rgbimg
 
 
-def get_rgb(image, N, w, h):
-    print("get rgb")
-    div = w // N
-    rgbimg = []
-    htile = 0
-
-    while htile < h:
-        wtile = 0
-        while wtile < w:
-            r, g, b = get_average_color(wtile, htile, div, image)
-            rgbimg += [(r, g, b)]
-            wtile += div
-        htile += div
-    print("DONE getrgb")
-    return rgbimg
-
-
-def get_average_color(w, h, n, image):
-    """ Returns a 3-tuple containing the RGB value of the average color of the
-    given square bounded area of length = n whose origin (top left corner) 
-    is (x, y) in the given image"""
-    print("G_A_C")
-    print(w, h)
-    image = Image.open(image).load()
-    r, g, b = 0, 0, 0
-    count = 0
-    for s in range(w, w + n):
-        for t in range(h, h + n):
-            pixlr, pixlg, pixlb = image[s, t]
-            r += pixlr
-            g += pixlg
-            b += pixlb
-            count += 1
-    print("DONE GAC")
-    return ((r // count), (g // count), (b // count))
-
-
-
-
 def grid(nj, orgimage):
-    print("GRID")
     mosaic_images = [f for f in listdir(mypath + "mosaic_photos/") if isfile(
         join(mypath + "mosaic_photos/", f)) if f != '.DS_Store']
-    mergeSort(mosaic_images)
-    print("LIST WITH IMAGES")
+    mosaic_images.sort()
     tile = Image.open(mypath + "mosaic_photos/" + mosaic_images[0])
     w, h = tile.size  # width and height of tile
-    print(w, h)
     orgimage = Image.open(orgimage)
     total_w, total_h = orgimage.size
-    print(total_w, total_h)
-    x = 0
+    # x = 0
     y = 0
     t = 0
     result = Image.new('RGB', (total_w, total_h))  # new image
