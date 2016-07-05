f94eb7fc494905145bba543a00f131f7d91c4914
Alessia Nardotto <alessianard8@gmail.com>
2016-07-05 10:54:08 +0100

    Update and rename assigning/munkres_test.py to assigning/backups/munkres_test.py

--- assigning/backups/munkres_test.py
+++ assigning/backups/munkres_test.py
@@ -1,11 +1,9 @@
 from munkres import Munkres
 from ColourCost import *
 
-#returns the list of tuples [(row,col), (row,col)] of the matching
+# returns the list of tuples [(row,col), (row,col)] of the matching
 def Final(tup):
     inputtable = DiffTable(tup)
     m = Munkres()
     return m.compute(inputtable)
-
-
-   
+    
