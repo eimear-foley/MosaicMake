0596e8cbfade054dd03a5abe7bc13cdb49f7bcc7
Eimear Foley <eimearfoley96@gmail.com>
2016-07-06 11:46:42 +0100

    Update try.py

--- GUI/try.py
+++ GUI/try.py
@@ -46,10 +46,10 @@ if http_cookie_header:
             title = "Upload your photo!"
 		    result = """<form enctype="multipart/form-data" 
                         action="save_file.py" method="post">
-   				<p>File: <input type="file" name="filename" /></p>
-   				<p><input type="submit" value="Upload" /></p>
-   				</form>
-				"""
+   				        <p>File: <input type="file" name="filename" /></p>
+   						<p><input type="submit" value="Upload" /></p>
+   						</form>
+						"""
 
 print("Content-Type: text/html")
 print()
