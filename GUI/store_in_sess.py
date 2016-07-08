919d65504f70488757f881afdcd56e098a420742
Catherine Bowen <catherinebowen96@gmail.com>
2016-07-08 12:28:34 +0100

    Add files via upload

---
+++ GUI/store_in_sess.py
@@ -0,0 +1,20 @@
+#!/usr/lib/env python3
+
+from cgi import FieldStorage, escape
+from cgitb import enable
+enable()
+from shelve import open
+from http.cookies import SimpleCookie
+
+form_data = FieldStorage()
+if len(form_data) != 0:
+	token = escape(form_data.get_first("token", "").strip())
+	cookie = SimpleCookie()
+	cookie['token'] = token
+	session_store = open("sessions/sess_" + token, writeback=True)
+    session_store['authenticated'] = True
+    session_store['token'] = token
+    session_store.close()
+    print("good")
+else:
+	print("problem")
