90ee42daacc62034e3fb7948bf9dce8ff9cb819f
Catherine Bowen <catherinebowen96@gmail.com>
2016-07-08 12:08:04 +0100

    Create store_accesstoken.js

---
+++ GUI/store_accesstoken.js
@@ -0,0 +1,25 @@
+
+//to be called in another function with accesstoken as the parameter
+function call_mosaic_make(accesstoken){
+	var url = "store_in_sess.py?token=" + acesstoken;
+	request = new XMLHttpRequest();
+ 	request.addEventListener('readystatechange', handle_response1, false);
+ 	request.open('GET', url, true);
+ 	request.send(null);
+}
+
+
+//if all's good redirect to try.py
+function handle_response1(){
+  if ( request.readyState === 4 ) {
+        if ( request.status === 200 ) {
+            if(request.responseText.trim() === 'problem') {
+	      		console.log("ERROR!");
+	      		//something to show user stuff didn't work D:
+	      	} else if (request.responseText.trim() === 'good'){
+	      		window.location.href = "try.py";
+
+	     	}
+	    }
+	}
+}  
