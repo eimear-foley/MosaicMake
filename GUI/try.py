ba3aa47bf7e41faee455a0101838bdb6a9bf9cb9
Alessia Nardotto <alessianard8@gmail.com>
2016-07-12 14:40:14 +0100

    Update try.py

--- GUI/try.py
+++ GUI/try.py
@@ -1,6 +1,5 @@
 #!/usr/local/bin/python3
 
-
 from cgi import FieldStorage, escape
 from cgitb import enable
 enable()
@@ -13,23 +12,19 @@ from api import *
 cookie = SimpleCookie()
 http_cookie_header = environ.get("HTTP_COOKIE")
 if http_cookie_header:
-    cookie.load(http_cookie_header)
-    if "token" in cookie:
-        token = cookie["token"].value
-        session_store = open("sessions/sess_" + token, writeback = False)
-        if session_store.get("authenticated"):
-	    
-            fileitem = graph.get_connections(profile['id'], 'picture')
-            si = SplitImage(fileitem, 60)
-            grid(Final(si), 'tmp_fold/usr_/' + token + '/resized.png', token)
-            source = 'tmp_fold/usr_'+ token + '/final.png'
-            
-            
-   	    else:
-            #they aren't logged in
-            source = 'Problem'
-            
-
+	cookie.load(http_cookie_header)
+	if "token" in cookie:
+		token = cookie["token"].value
+		session_store = open("sessions/sess_" + token, writeback = False)
+		if session_store.get("authenticated"):
+			fileitem = graph.get_connections(profile['id'], 'picture')
+			si = SplitImage(fileitem, 60)
+			grid(Final(si), 'tmp_fold/usr_/' + token + '/resized.png', token)
+			source = 'tmp_fold/usr_'+ token + '/final.png'
+		else:
+			# they aren't logged in
+			source = 'Problem'
+			
 print("Content-Type: text/plain")
 print()
 print(source)
