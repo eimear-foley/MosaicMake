47152ec8ed69a1585660333f11ce480a4f7ac1cf
Alessia Nardotto <alessianard8@gmail.com>
2016-07-27 15:23:31 +0100

    Update store_in_sess.py

--- GUI/store_in_sess.py
+++ GUI/store_in_sess.py
@@ -1,20 +1,25 @@
-#!/usr/lib/env python3
+#!/usr/bin/env python3
 
 from cgi import FieldStorage, escape
 from cgitb import enable
 enable()
 from shelve import open
 from http.cookies import SimpleCookie
-
 form_data = FieldStorage()
+
 if len(form_data) != 0:
-	token = escape(form_data.get_first("token", "").strip())
+	token = form_data.getfirst("token")
 	cookie = SimpleCookie()
 	cookie['token'] = token
-	session_store = open("sessions/sess_" + token, writeback=True)
+	session_store = open("/var/www/html/sessions/sess_" + token, writeback=True)
 	session_store['authenticated'] = True
 	session_store['token'] = token
 	session_store.close()
+	print(cookie)
+	print("Content-Type: text/plain")
+	print()
 	print("good")
 else:
+	print("Content-Type: text/plain")
+	print()
 	print("problem")
