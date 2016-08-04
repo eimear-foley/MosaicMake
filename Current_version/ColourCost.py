75be988f8bdd5fcdb0642f2e378ea704ca173cfd
gabystephanov <115342151@umail.ucc.ie>
2016-08-04 13:56:16 +0100

    Create ColourCost.py

---
+++ Current_version/ColourCost.py
@@ -0,0 +1,26 @@
+from math import sqrt
+
+def ColourDiff(tup1,tup2):
+
+    r1,g1,b1 = tup1
+    r2,g2,b2 = tup2
+    rChange = (r1 - r2) ** 2
+    gChange = (g1 - g2) ** 2
+    bChange = (b1 - b2) ** 2
+    rMean = (r1 + r2) // 2
+
+    return int(round(sqrt((2 + rMean // 256 ) * rChange + 4 * gChange + (2 + (255 - rMean) // 256) * bChange)))
+
+def DiffTable(tup):
+
+    (lt1,lt2) = tup
+    table = []
+
+    for colour1 in lt1:
+        row = []
+        for colour2 in lt2:
+            row += [ColourDiff(colour1,colour2)]
+        table += [row]
+
+    return table
+
