307b7904803a1b2da72684e5a9fc51714c6928fd
Alessia Nardotto <alessianard8@gmail.com>
2016-08-09 09:51:38 +0100

    Update store_in_sess.py

--- GUI/store_in_sess.py
+++ GUI/store_in_sess.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from cgi import FieldStorage, escape
+import os
 from cgitb import enable
 enable()
 from shelve import open
@@ -8,18 +9,20 @@ from http.cookies import SimpleCookie
 form_data = FieldStorage()
 
 if len(form_data) != 0:
-	token = form_data.getfirst("token")
-	cookie = SimpleCookie()
-	cookie['token'] = token
-	session_store = open("/var/www/html/sessions/sess_" + token, writeback=True)
-	session_store['authenticated'] = True
-	session_store['token'] = token
-	session_store.close()
-	print(cookie)
-	print("Content-Type: text/plain")
-	print()
-	print("good")
+        token = form_data.getfirst("token")
+        cookie = SimpleCookie()
+        cookie['token'] = token
+        cookie['mode'] = 'facebook'
+        mypath = '/var/www/html/tmp_fold/usr_' + token
+        if not os.path.isdir(mypath):
+                os.makedirs(mypath)
+                os.chmod(mypath, 0o777)
+
+        print(cookie)
+        print("Content-Type: text/plain")
+        print()
+        print("good")
 else:
-	print("Content-Type: text/plain")
-	print()
-	print("problem")
+        print("Content-Type: text/plain")
+        print()
+        print("problem")
