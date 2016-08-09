ac314ea232f2370f3d4433f00ea095f5568422fd
Alessia Nardotto <alessianard8@gmail.com>
2016-08-09 09:54:19 +0100

    Update try.py

--- GUI/try.py
+++ GUI/try.py
@@ -14,9 +14,9 @@ from PIL import Image
 import tempfile
 from nj import *
 from cgi import FieldStorage, escape
-
 os.environ['http_proxy']="http://4c.ucc.ie:80"
 os.environ['https_proxy']="http://4c.ucc.ie:80"
+
 
 form_data = FieldStorage()
 photos = escape(form_data.getfirst("photos","").strip())
@@ -26,50 +26,23 @@ opacity = escape(form_data.getfirst("opacity","").strip())
 cookie = SimpleCookie()
 http_cookie_header = environ.get("HTTP_COOKIE")
 if http_cookie_header:
-	cookie.load(http_cookie_header)
-	if "token" in cookie:
-		token = cookie["token"].value
-		source = token
-		usr_fold = '/var/www/html/tmp_fold/usr_' + token
-		session_store = open("/var/www/html/sessions/sess_" + token, writeback = False)
-		if session_store.get('authenticated'):
-			try:
-				temp = os.makedirs(usr_fold, mode = 0o777)
-				os.chmod(usr_fold, 0o777)
-				temp = os.makedirs(usr_fold + '/images', mode = 0o777)
-				os.chmod(usr_fold + '/images', 0o777)
-			except FileExistsError:
-				source = "Error"
-			user = "me"
-			graph = facebook.GraphAPI(token)
-			profile = graph.get_object(user)
-			fileitem = graph.get_connections(profile['id'], 'picture', width=9999)
-			response = requests.get(fileitem['url'])
-			img = Image.open(BytesIO(response.content))
-			img.save(usr_fold + '/profile.png')
-			si = SplitImage(usr_fold + '/profile.png', int(photos), token, tags)
-			source = grid(Final(si), usr_fold + '/resized.png', token, opacity)                   
-			source = '../tmp_fold/usr_'+ token + '/final.png'
-			print('Content-Type: text/plain')
-			print()
-			print(source)
-		# else:
-			# they aren't logged in
-			# source = 'Problem'
-	elif "up_token" in cookie:
-		token = cookie["up_token"].value
-		source = token
-		usr_fold = '/var/www/html/tmp_fold/usr_' + token
-		try:
-			temp = os.makedirs(usr_fold, mode = 0o777)
-			os.chmod(usr_fold, 0o777)
-			temp = os.makedirs(usr_fold + '/images', mode = 0o777)
-			os.chmod(usr_fold + '/images', 0o777)
-		except FileExistsError:
-			source = "Error"
-		si = SplitImage(usr_fold + '/profile.png', int(photos), token, tags)
-		source = grid(Final(si), usr_fold + '/resized.png', token, opacity)
-		source = '../tmp_fold/usr_'+ token + '/final.png'
-		print('Content-Type: text/plain')
-		print()
-		print(source)
+        cookie.load(http_cookie_header)
+        if "token" in cookie:
+                token = cookie["token"].value
+                source = token
+                usr_fold = '/var/www/html/tmp_fold/usr_' + token
+                try:
+
+                        temp = os.makedirs(usr_fold, mode = 0o777)
+                        os.chmod(usr_fold, 0o777)
+                        temp = os.makedirs(usr_fold + '/images', mode = 0o777)
+                        os.chmod(usr_fold + '/images', 0o777)
+                except FileExistsError:
+                        source = "Error"
+                si = SplitImage(usr_fold + '/profile.png', int(photos), token, tags)
+                source =si
+                source = grid(Final(si), usr_fold + '/resized.png', token, opacity)
+                source = '../tmp_fold/usr_'+ token + '/final.png'
+                print('Content-Type: text/plain')
+                print()
+                print(source)
