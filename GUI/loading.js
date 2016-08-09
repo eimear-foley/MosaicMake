0f4dfa18ad30d0038c185284b099e65ad5484068
Alessia Nardotto <alessianard8@gmail.com>
2016-08-09 09:45:58 +0100

    Update loading.js

--- GUI/loading.js
+++ GUI/loading.js
@@ -1,23 +1,19 @@
-
 (function(){
-
+  
 var request;
 document.addEventListener("DOMContentLoaded", init, false);
 
 function init(){
 console.log("init");
-    wrapper  = document.querySelector('#wrapper');
+    image  = document.querySelector('#wrapper');
 call_try();
 
 }
 
-
-
-
 //to be called in another function with accesstoken as the parameter
 function call_try(){
-        var currentlocation = window.location;
-        console.log(currentlocation);
+        var currentlocation = window.location;                                                                           
+        console.log(currentlocation);                                                                                       
         console.log(currentlocation.search);
         console.log("call_try");
         var url = "try.py" + currentlocation.search;
@@ -42,26 +38,22 @@ console.log("1");
                         console.log("ERROR!");
                         //something to show user stuff didn't work D:
                 } else {
-                        var button = "<button onclick='saveFile(" + "'" + request.responseText.trim() + "'" +  ")'>save</button>";
-                        image = document.createElement('img');
-                        image.innerHTML = "<img src="+"'" + request.responseText.trim() + "'>" + button;
-                        wrapper.appendChild(image);
-                        wrapper.style.display = 'block';
-                        wrapper.style.height = '70%';
-                        wrapper.style.width = '100%';
-                        wrapper.style.border = '1px solid black';
-                        wrapper.style.backgroundColor = 'white';
-                        wrapper.style.marginLeft = 'auto';
-                        wrapper.style.marginRight = 'auto';
+                        image.innerHTML = "<div><img src="+ "'" + request.responseText.trim() + "'></div>"; 
+                        image.style.marginLeft = 'auto';
+                        image.style.marginRight = 'auto';
+                        image.style.display = 'block';
+                        image.style.height = '80%';
+                        image.style.width = '100%';
+                        image.style.border = '1px solid black';
+                        image.style.backgroundColor = 'white';
 
                 }
-                
             } else {
                 console.log("response = " + request.responseText.trim());
                 image.innerHTML = "<div style='margin-left: auto; margin-right: auto; height: 40%; width: 40%; padding: 1em;'><p>Sorry we are experiencing problems right now, please try again later!</p><br /><a href='http://143.239.81.202'>Return to the main page</a></div>"
         }
-        }
-}
+    }
+}  
 
 
 })();
