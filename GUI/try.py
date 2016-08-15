b25b29d550e26393ead9524f4f70a712320c58f8
Alessia Nardotto <alessianard8@gmail.com>
2016-08-15 09:55:49 +0100

    Update try.py

--- GUI/try.py
+++ GUI/try.py
@@ -6,6 +6,7 @@ from http.cookies import SimpleCookie
 from os import environ
 from shelve import open
 from mosaic import *
+from mosaic_demo import *
 import facebook
 import requests
 import os
@@ -14,13 +15,14 @@ from PIL import Image
 import tempfile
 from nj import *
 from cgi import FieldStorage, escape
-os.environ['http_proxy'] = "http://4c.ucc.ie:80"
-os.environ['https_proxy'] = "http://4c.ucc.ie:80"
+os.environ['http_proxy']="http://4c.ucc.ie:80"
+os.environ['https_proxy']="http://4c.ucc.ie:80"
 
 
 form_data = FieldStorage()
 photos = escape(form_data.getfirst("photos","").strip())
 tags = form_data.getlist("tags")
+premade = escape(form_data.getfirst("premade", "").strip())
 opacity = escape(form_data.getfirst("opacity","").strip())
 
 cookie = SimpleCookie()
@@ -31,17 +33,35 @@ if http_cookie_header:
                 token = cookie["token"].value
                 source = token
                 usr_fold = '/var/www/html/tmp_fold/usr_' + token
-                try:
-                        temp = os.makedirs(usr_fold, mode = 0o777)
-                        os.chmod(usr_fold, 0o777)
-                        temp = os.makedirs(usr_fold + '/images', mode = 0o777)
-                        os.chmod(usr_fold + '/images', 0o777)
-                except FileExistsError:
-                        source = "Error"
-                si = SplitImage(usr_fold + '/profile.png', int(photos), token, tags)
-                source = si
-                source = grid(Final(si), usr_fold + '/resized.png', token, opacity)
-                source = '../tmp_fold/usr_'+ token + '/final.png'
-                print('Content-Type: text/plain')
-                print()
-                print(source)
+                if premade == "false":
+                        try:
+                                temp = os.makedirs(usr_fold, mode = 0o777)
+                                os.chmod(usr_fold, 0o777)
+                                temp = os.makedirs(usr_fold + '/images', mode = 0o777)
+                                os.chmod(usr_fold + '/images', 0o777)
+                        except FileExistsError:
+                                source = "Error"
+                        si = SplitImage(usr_fold + '/profile.png', int(photos), token, tags)
+                        source = si
+                        if si == 'Tags no good':
+                                source = 'There was a problem with the tags chosen. Please try again with different tags.'
+                        else:
+                                source = grid(Final(si), usr_fold + '/resized.png', token, opacity)
+                                source = '../tmp_fold/usr_'+ token + '/final.png'
+                        print('Content-Type: text/plain')
+                        print()
+                        print(source)
+                elif premade == "true":
+                        path = '%sby%s' %(photos, photos)
+                        si = SplitImage2(usr_fold + '/profile.png', int(photos), token)
+                        source = si
+                        source = grid2(Final(si), usr_fold + '/resized.png', token, opacity, int(photos))
+                        source = '../tmp_fold/usr_'+ token + '/final.png'
+                        print('Content-Type: text/plain')
+                        print()
+                        print(source)
+                else:
+                        source = premade
+                        print('Content-Type: text/plain')
+                        print()
+                        print(source)
