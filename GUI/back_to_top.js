3e08fc70ac870ab6bc3e82cdaa4d7805696419bf
Alessia Nardotto <alessianard8@gmail.com>
2016-08-15 09:57:13 +0100

    Update back_to_top.js

--- GUI/back_to_top.js
+++ GUI/back_to_top.js
@@ -4,29 +4,29 @@ var back_to_top;
 window.addEventListener('DOMContentLoaded', init, false);
 
 function init(){
-	back_to_top = document.querySelector(".back-to-top");
-	window.addEventListener('scroll', show_button, false);
-	//back_to_top.addEventListener('mouseover', touching, false);
-	//back_to_top.addEventListener('mouseout', away, false);
-	back_to_top.style.display = "none";
+        back_to_top = document.querySelector(".back-to-top");
+        window.addEventListener('scroll', show_button, false);
+        //back_to_top.addEventListener('mouseover', touching, false);
+        //back_to_top.addEventListener('mouseout', away, false);
+        back_to_top.style.display = "none";
 };
 
 
 
 function show_button(){
-	var travelled = window.pageYOffset;
+        var travelled = window.pageYOffset;
 
-	if (travelled > amountScrolled){
-		back_to_top.style.display = "block";
-	} else {
-		back_to_top.style.display = "none";
-	}
+        if (travelled > amountScrolled){
+                back_to_top.style.display = "block";
+        } else {
+                back_to_top.style.display = "none";
+        }
 };
 
 /*function touching(){
-	back_to_top.style.opacity = "1";
+        back_to_top.style.opacity = "1";
 };
 
 function away(){
-	back_to_top.style.opacity = ".5";
+        back_to_top.style.opacity = ".5";
 }*/
