9485455171dbc86460cda3a19738b2f26a69c965
Alessia Nardotto <alessianard8@gmail.com>
2016-08-15 09:41:13 +0100

    Update flickr_api.py

--- api/flickr_api.py
+++ api/flickr_api.py
@@ -8,8 +8,9 @@ os.environ['https_proxy']="http://4c.ucc.ie:80"
 
 
 def getPhotos(token, tags_list, tileWidth):
-    path = '/var/www/html/tmp_fold/usr_'+token+'/images/'
-    #path = '/var/www/html/demo/'
+    if len(tags_list) == 0:
+        return 'Go away'
+    path = '/var/www/html/tmp_fold/usr_' + token + '/images/'
     if not os.path.isdir(path):
         os.makedirs(path)
         os.chmod(path, 0o777)
@@ -19,21 +20,34 @@ def getPhotos(token, tags_list, tileWidth):
     flickr = flickrapi.FlickrAPI(api_key, api_secret, store_token=False)
     limit = 0
     count = 0
+    failures = 0
     for tag in tags_list:
-        limit += 25
-        for photo in flickr.walk(tag_mode='all', tags=tag):
-            count += 1
-            if count <= limit:
-                photo_id = photo.get('id')
-                farm_id = photo.get('farm')
-                server = photo.get('server')
-                secret = photo.get('secret')
-                url = 'http://farm' + farm_id + '.staticflickr.com/' + server + '/' + photo_id + '_' + secret + '.jpg'
-                im = urllib.request.urlretrieve(url, path + 'test.png')
-                im = Image.open(path + 'test.png')
-                im = im.resize((tileWidth, tileWidth), Image.ANTIALIAS)
-                im.save(path + '%s%s%s' % ('photo',count,'.png'), subsampling = 0, quality = 100)
-                #im.save(path + '%sby%s' + '%s%s%s' % (num, num, 'photo',count,'.png'), subsampling = 0, quality = 100)
-            else:
-                break
-    return
+        worked = False
+        limit += 25 
+        try:
+            for photo in flickr.walk(tag_mode='all', tags=tag):
+                worked = True
+                count += 1
+                if photo == "None":
+                    failures += 1
+                    break 
+                if count <= limit:
+                    photo_id = photo.get('id')
+                    farm_id = photo.get('farm')
+                    server = photo.get('server')
+                    secret = photo.get('secret')
+                    url = 'http://farm' + farm_id + '.staticflickr.com/' + server + '/' + photo_id + '_' + secret + '.jpg'
+                    im = urllib.request.urlretrieve(url, path + 'test.png')
+                    im = Image.open(path + 'test.png')
+                    im = im.resize((tileWidth, tileWidth), Image.ANTIALIAS)
+                    im.save(path + '%s%s%s' % ('photo',count,'.png'), subsampling = 0, quality = 100)
+                else:
+                    break
+            if not worked:
+                failures += 1        
+        except:
+            failures += 1
+            continue
+    if failures == len(tags_list):
+        return 'Go away'
+    return 'gr8' 
