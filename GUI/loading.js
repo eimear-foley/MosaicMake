4ec04796cf3bd6982658f72cd2bc8e8a76929aed
Alessia Nardotto <alessianard8@gmail.com>
2016-08-15 10:05:52 +0100

    Update loading.js

--- GUI/loading.js
+++ GUI/loading.js
@@ -12,8 +12,8 @@ call_try();
 
 //to be called in another function with accesstoken as the parameter
 function call_try(){
-        var currentlocation = window.location;                                                                           
-        console.log(currentlocation);                                                                                       
+        var currentlocation = window.location;
+        console.log(currentlocation);
         console.log(currentlocation.search);
         console.log("call_try");
         var url = "try.py" + currentlocation.search;
@@ -37,7 +37,9 @@ console.log("1");
             if(request.responseText.trim() === 'problem') {
                         console.log("ERROR!");
                         //something to show user stuff didn't work D:
-                } else {
+            } else if(request.responseText.trim() === 'There was a problem with the tags chosen. Please try again with different tags.'){
+                        image.innerHTML = "<div><p>" + request.responseText.trim() + "</p></div>"
+        } else {
                         image.innerHTML = "<div><img src="+ "'" + request.responseText.trim() + "'></div>"; 
                         image.style.marginLeft = 'auto';
                         image.style.marginRight = 'auto';
@@ -52,7 +54,7 @@ console.log("1");
                 console.log("response = " + request.responseText.trim());
                 image.innerHTML = "<div style='margin-left: auto; margin-right: auto; height: 40%; width: 40%; padding: 1em;'><p>Sorry we are experiencing problems right now, please try again later!</p><br /><a href='http://143.239.81.202'>Return to the main page</a></div>"
         }
-    }
+        }
 }  
 
 
