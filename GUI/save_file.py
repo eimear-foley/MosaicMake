ad67cf3ea9eed183002d7363e679f29704758b9f
Alessia Nardotto <alessianard8@gmail.com>
2016-08-23 10:40:11 +0100

    Update save_file.py

--- GUI/save_file.py
+++ GUI/save_file.py
@@ -10,7 +10,6 @@ from shelve import open
 from PIL import Image
 from http.cookies import SimpleCookie
 
-
 sid = sha256(repr(time()).encode()).hexdigest()
 
 cookie = SimpleCookie()
@@ -27,22 +26,16 @@ if len(form_data) != 0:
         fileitem = form_data['filename']
         # Test if the file was uploaded
         if fileitem.filename:
-                # strip leading path from file name to avoid 
-                # directory traversal attacks
-                # fn = os.path.basename(fileitem.filename.replace("\\", "/" ))
-                # sid = sha256(repr(time()).encode()).hexdigest()
                 mypath = '/var/www/html/tmp_fold/usr_' + sid
                 if not os.path.isdir(mypath):
                         os.makedirs(mypath)
                         os.chmod(mypath, 0o777)
-                # open(mypath + '/' + fn, 'wb').write(fileitem.file.read())
                 img = Image.open(fileitem.file)
                 img.convert('RGB')
                 img.save(mypath + '/profile.png')
                 os.chmod(mypath + '/profile.png',0o777)
                 status = True
 else:
-        # output = 'Please upload a file'
         status = False
 if status:
         print("Location:http://143.239.81.202/cgi-bin/resize.py")
@@ -51,26 +44,14 @@ print()
 
 print("""
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by TEMPLATED
-http://templated.co
-Released for free under the Creative Commons Attribution License
-
-Name       : RedMarket 
-Description: A two-column, fixed-width design with dark color scheme.
-Version    : 1.0
-Released   : 20140101
-
--->
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Webpage</title>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
+<title>MosaicMake</title>
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
 <link href="../default.css" rel="stylesheet" type="text/css" media="all" />
 <link href="../fonts.css" rel="stylesheet" type="text/css" media="all" />
+<link rel="icon" href="../jigsaw.png">
 
 <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
 
