52b65ee95e82e41b12d3c1d7849a262ed9e87860
Eimear Foley <eimearfoley96@gmail.com>
2016-11-21 22:38:48 +0000

    Delete munkres_test.py

--- assigning/backups/munkres_test.py
+++
@@ -1,9 +0,0 @@
-from munkres import Munkres
-from ColourCost import *
-
-# returns the list of tuples [(row,col), (row,col)] of the matching
-def Final(tup):
-    inputtable = DiffTable(tup)
-    m = Munkres()
-    return m.compute(inputtable)
-    
